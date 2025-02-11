f927d1396d639a09a1c8a67d7a0ad34ff0fd57ce
root <whqkrqkr12345@gmail.com>
2025-02-11 16:04:18 +0900

    c_2309

--- c_2309/main.swift
+++ c_2309/main.swift
@@ -1,11 +1,19 @@
-//
-//  main.swift
-//  c_2309
-//
-//  Created by 조근호 on 2/11/25.
-//
-
 import Foundation
 
-print("Hello, World!")
+var num = [Int]()
 
+for _ in 0..<9 {
+    num.append(Int(readLine()!)!)
+}
+
+outerLoop: for i in 0..<9 {
+    for j in i+1..<9 {
+        if num.reduce(0,+) - (num[i] + num[j]) == 100 {
+            num.remove(at: i)
+            num.remove(at: j-1)
+            break outerLoop
+        }
+    }
+}
+
+for i in num.sorted() { print(i) }
